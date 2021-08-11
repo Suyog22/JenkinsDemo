1d55461bb3cbc3f2b330afa0d9bbcb79388db9b4
Suyog Majgaonkar <suyog.majgaonkar@standardbank.co.za>
2021-08-11 18:28:52 +0530

    Updated main file

--- src/main/java/com/example/covid/main/SpringBootCovidDemo.java
+++ src/main/java/com/example/covid/main/SpringBootCovidDemo.java
@@ -8,7 +8,7 @@ public class SpringBootCovidDemo {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Hi...123");
+		System.out.println("Hi...12345");
 		SpringApplication.run(SpringBootCovidDemo.class, args);
 	}
 
