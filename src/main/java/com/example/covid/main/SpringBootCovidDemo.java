b357368de86cdddab710bf7c07656a7b5bfdea7b
Suyog Majgaonkar <suyog.majgaonkar@standardbank.co.za>
2021-08-11 17:12:41 +0530

    added new statement in main file

--- src/main/java/com/example/covid/main/SpringBootCovidDemo.java
+++ src/main/java/com/example/covid/main/SpringBootCovidDemo.java
@@ -8,7 +8,7 @@ public class SpringBootCovidDemo {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Helloooooooooooooooooooooooo");
+		System.out.println("Helloooooooooooooooooooooooo123");
 		SpringApplication.run(SpringBootCovidDemo.class, args);
 	}
 
